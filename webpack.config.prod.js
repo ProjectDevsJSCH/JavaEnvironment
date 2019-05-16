b0aee8bc7cd3127e01f01a825e01819e6a53939b
jscastelblancoh <juan.castelblanco@exsis.loc>
2019-05-16 14:42:08 -0500

    Module 13

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,13 +1,16 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 
 export default {
     debug: true,
-    devtool: 'source map',
+    devtool: 'source-map',
     nonfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+        main: path.resolve(__dirname, 'src/index'),
+        vendor: path.resolve(__dirname, 'src/vendor')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -15,8 +18,27 @@ export default {
         filename: 'bundle.js'
     },
     plugins: [
+        //Create HTML file that includes reference to bundled JS
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            },
+            inject: true
+        }),
+        //Eliminate duplicated packages whe generating bundle
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        //Minify JS
+        new webpack.optimize.UglifyJsPlugin(),
     ],
     module: {
         loaders: [
