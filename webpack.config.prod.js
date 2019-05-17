7b182f57fd4a228de088dbdbf043eab134a2f4df
jscastelblancoh <juan.castelblanco@exsis.loc>
2019-05-17 08:59:42 -0500

    Module finished

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,8 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
     debug: true,
@@ -15,9 +16,23 @@ export default {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js',
+
     },
     plugins: [
+
+        // Generate an external css file with a hash un the filename
+        new ExtractTextPlugin('[name].[contenthash].css'),
+
+        // Hash the files using MD5 so that their names change when the content changes
+        new WebpackMd5Hash(),
+
+        // Use CommonsChunkPlugin to create a separate bundle
+        // of vendor libraries so that they are cached separately
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+
         //Create HTML file that includes reference to bundled JS
         new HtmlWebpackPlugin({
             template: 'src/index.html',
@@ -33,17 +48,20 @@ export default {
                 minifyCSS: true,
                 minifyURLs: true
             },
-            inject: true
+            inject: true,
+            trackJSToken: "asdasdasd"
         }),
+
         //Eliminate duplicated packages whe generating bundle
         new webpack.optimize.DedupePlugin(),
+
         //Minify JS
         new webpack.optimize.UglifyJsPlugin(),
     ],
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
         ]
     }
 }
